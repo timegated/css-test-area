cfb62eb281bec3bfa9c5dccb365c5e809bbd06e4
dbeccaria826 <beccdaniel@gmail.com>
2020-09-03 09:47:04 -0400

    added media-queries section to css-the-complete-guide

--- css-the-complete-guide/3-course-project-customers/script.js
+++ css-the-complete-guide/3-course-project-customers/script.js
@@ -1,15 +1,30 @@
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
+const noModal = document.querySelector('.modal__action--negative');
 const planButton = Array.from(document.querySelectorAll('.button'));
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
-window.addEventListener('click', (e) => {
-  backdrop.style.display = 'none';
-});
+console.dir(backdrop);
+const closeModal = () => {
+  backdrop.classList.remove('open')
+  modal.classList.remove('open')
+};
+if (modalNoButton) {
+  noModal.addEventListener('click', closeModal);
+}
 
 planButton.forEach(button => {
   button.addEventListener('click', (e) => {
-    backdrop.style.display = 'block';
-    modal.style.display = 'block';
+    // backdrop.style.display = 'block';
+    // modal.style.display = 'block';
+    modal.classList.add('open');
+    backdrop.classList.add('open');
     e.stopPropagation();
   });
 });
+
+toggleButton.addEventListener(('click'), () => {
+  mobileNav.style.display = 'block';
+  backdrop.style = 'block';
+})
